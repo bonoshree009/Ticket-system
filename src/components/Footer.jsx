6cc8a23f60d2205c5e18f8bdefc8202de7ba9f2f
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-10-16 01:17:19 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,43 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 ">
+  <nav>
+    <h6 className="footer-title">CS — Ticket System</h6>
+    <p >Lorem Ipsum is simply dummy text of the <br /> printing and typesetting industry. Lorem  <br />
+    Ipsum has been the industry's standard <br />dummy text ever since the 1500s, when an <br />unknown printer took a galley of type and <br /> scrambled it to make a type specimen book.</p>
+
+  </nav>
+  <nav className='flex-1'>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">Our Mission</a>
+    <a className="link link-hover"> Contact Saled</a>
+
+  </nav>
+  <nav>
+    <h6 className="footer-title">Services</h6>
+    <a className="link link-hover">Products & Services</a>
+    <a className="link link-hover">Customer Stories</a>
+    <a className="link link-hover">Download Apps</a>
+  </nav>
+
+    <nav>
+    <h6 className="footer-title">Information</h6>
+    <a className="link link-hover">Privacy Policy</a>
+    <a className="link link-hover">Terms & Conditions</a>
+    <a className="link link-hover">Join Us</a>
+  </nav>
+    <nav>
+    <h6 className="footer-title">Social Links</h6>
+    <a className="link link-hover">@CS — Ticket System</a>
+    <a className="link link-hover">@CS — Ticket System</a>
+    <a className="link link-hover">@CS — Ticket System</a>
+    <a className="link link-hover">support@cst.com</a>
+  </nav>
+</footer>
+<div className='bg-black text-white text-center py-5 border-t-1 border-gray-600'><p>© 2025 CS — Ticket System. All rights reserved.</p></div>
+</>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Navbar = () => {
   return (
 
-    <div className="navbar bg-[#F3E8FF] shadow-sm mb-8">
+    <div className="navbar bg-[#F3E8FF] shadow-sm mb-8 md:px-14 px-1">
   <div className="navbar-start">
+
+
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -18,19 +20,26 @@ const Navbar = () => {
           <li><a>Contact</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">Univarsity DashBoard</a>
+
+
+    <a className="btn btn-ghost text-xl font-bold">CS — Ticket System</a>
   </div>
-  <div className="navbar-center hidden lg:flex">
+
+  
+  <div className="navbar-end">
+    <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
            <li><a>Home</a></li>
-          <li><a>Faculty</a></li>
-          <li><a>Students</a></li>
-          <li><a>Contact</a></li>
+          <li><a>FAQ</a></li>
+          <li><a>Changelog</a></li>
+          <li><a>Blog</a></li>
+           <li><a>Download</a></li>
+            <li><a>Contact</a></li>
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn bg-blue-400">+New Assignment</a>
+     <a className="btn bg-linear-to-r  from-purple-700 to-purple-500 text-white">+ New Ticket</a>
   </div>
+
 </div>
          
   );
